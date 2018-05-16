bf8a0c3a0a04eb8df8b5e766a0bacdf06d82468b
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2018-05-16 23:55:36 +0800

    static files
    ugh heroku

--- django_shortener/settings.py
+++ django_shortener/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
