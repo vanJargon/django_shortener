ef98d382199220d05cb61b9da82f053d7a61081c
vanJargon <vanessa_yun@mymail.sutd.edu.sg>
2018-05-17 00:04:37 +0800

    no diff

--- django_shortener/settings.py
+++ django_shortener/settings.py
@@ -128,9 +128,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
